eac5bce79bc17b3f2cb6c000b0cc8dfd7a08b19d
EsmeraldaSice <53306401+EsmeraldaSice@users.noreply.github.com>
2019-07-30 09:31:22 -0400

    Actualizacion login

--- APPSRSM/APPSRSM/APPSRSM/App.xaml.cs
+++ APPSRSM/APPSRSM/APPSRSM/App.xaml.cs
@@ -11,7 +11,7 @@ namespace APPSRSM
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
